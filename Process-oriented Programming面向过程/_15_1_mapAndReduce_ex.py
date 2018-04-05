a74008b7f60bb114b94f4c51fe45f039c4aa915f
liuzhu <lzworker@outlook.com>
2018-04-05 23:08:04 +0800

    map和reduce的应用
    str2float的使用

--- Process-oriented Programming面向过程/_15_1_mapAndReduce_ex.py
+++ Process-oriented Programming面向过程/_15_1_mapAndReduce_ex.py
@@ -33,3 +33,19 @@ else:
 利用map和reduce编写一个str2float函数，
 把字符串'123.456'转换成浮点数123.456
 '''
+def str2float(s):
+    DIGHTS = {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9}
+    dot = s.index('.')
+
+    def str2num(str):
+        return DIGHTS[str]
+
+    def fn(x,y):
+        return x*10+y
+    return reduce(fn,map(str2num,str[:dot]+str[dot+1:]))/10**dot
+
+print('str2float(\'123.456\') =', str2float('123.456'))
+if abs(str2float('123.456') - 123.456) < 0.00001:
+    print('1')
+else:
+    print('0')
